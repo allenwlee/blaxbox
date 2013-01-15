4e651c922fc71ab0518b3b17afb60975fd77ac29
Allen Lee <allenwlee130@gmail.com>
2013-01-14 19:50:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blaxbox::Application.config.secret_token = 'e62b067d4f811191387ac5fc154535350660a7dd41feb623cf7940808bc02867a9dd07bd5a78599316a636ec23b135ac12e4131c0bbc909b3ea15266114e1bad'

ee023ae033d6aafa879ecc0211f6400a0f7d6ee8
Allen Lee <allenwlee130@gmail.com>
2013-01-15 21:41:19 -0800

    average!!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blaxbox::Application.routes.draw do
   resources :financials
   
+  root to:  'financials#new'
+
   get "financials/new"
 
   get "advances/new"
@@ -56,7 +58,7 @@ Blaxbox::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to:  'static_pages#home'
+
 
   # See how all your routes lay out with "rake routes"
 
